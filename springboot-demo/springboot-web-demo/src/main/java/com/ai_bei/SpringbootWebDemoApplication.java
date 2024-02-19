f81e3c7d83843511dc3590434fca7022b33aee93
liuronghua <liuronghua@grgbanking.com>
2024-02-19 15:55:20 +0800

    1/ 创建SpringbootWebDemoApplication

---
+++ springboot-demo/springboot-web-demo/src/main/java/com/ai_bei/SpringbootWebDemoApplication.java
@@ -0,0 +1,22 @@
+package com.ai_bei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (C), 2010-2023
+ * FileName:     SpringbootWebDemoApplication
+ * Author:       shine
+ * Date:         2024/2/19 15:52
+ * Description:
+ * History:
+ * <author>      <time>          <version>          <desc>
+ * 作者姓名       修改时间         版本号             描述
+ */
+@SpringBootApplication
+public class SpringbootWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebDemoApplication.class);
+    }
+}
